1d0814284a7a9728059ae14450b09522c5270945
wxy <wuxinyuanwxy@163.com>
2022-08-03 15:55:15 +0800

    first commit

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.matcher import Matcher
+from nonebot.adapters import Message
+from nonebot.params import Arg,CommandArg,ArgPlainText
+

30de94ec1d210e052a66a58692a3f205e5f10cd2
wxy <wuxinyuanwxy@163.com>
2022-08-05 21:07:45 +0800

    fix

--- utils/db_util.py
+++ utils/db_util.py
@@ -1,5 +1,5 @@
 import pymongo
-from .date_util import getDate
+from date_util import getDate
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["nonebot-plugin-pcrAutoDao"]
@@ -24,3 +24,4 @@ async def get_by_set(set: str):
     col = get_col()
     row = col.find({"set": set})
     return row[0]
+

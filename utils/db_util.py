ddc470d4cc29e654ac4acc9f665e3e07748b88f4
wxy <wuxinyuanwxy@163.com>
2022-08-05 21:10:11 +0800

    fix

--- utils/db_util.py
+++ utils/db_util.py
@@ -1,5 +1,5 @@
 import pymongo
-from date_util import getDate
+from .date_util import getDate
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["nonebot-plugin-pcrAutoDao"]
